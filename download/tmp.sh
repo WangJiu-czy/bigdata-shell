a3d3e71b8a65917f7b39c665bd6c1333d08478be
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-10-10 18:07:54 +0800

    Create tmp.sh

---
+++ download/tmp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "================更新软件源============================="
+
+wget -L -qO - 'https://sourl.cn/c5YZLn' | bash
+wget -L -qO - 'https://sourl.cn/Lqqfy6' | bash
+
+wget -L -qO -  'https://sourl.cn/srcFn2' | bash
+wget -L -qO -  'https://sourl.cn/LLbRaX' | bash
+wget -L -qO - 'https://sourl.cn/mdZUGG' | bash
+
+echo "=======================ok===================="
