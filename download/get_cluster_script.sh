96e3615b88dc5b56981124d483a4f145a0bf47fa
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-10-20 13:48:10 +0800

    Update get_cluster_script.sh

--- download/get_cluster_script.sh
+++ download/get_cluster_script.sh
@@ -4,15 +4,15 @@ source ~/.bashrc
 
 Link="https://ghproxy.com/https://raw.githubusercontent.com/WangJiu-czy/bigdata-shell/main/init/xsync"
 
-wget -q -P $HADOOP_HOME/bin $Link -O $HADOOP_HOME/bin/xsync
+wget -q -P $HADOOP_HOME/bin $Link -O $HADOOP_HOME/bin/xsync --no-check-certificatev
 chmod +x $HADOOP_HOME/bin/xsync
 
-JPS="https://ghproxy.com/https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/jpsall"
+JPS="https://ghproxy.com/https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/jpsall" --no-check-certificate
 wget -q -P $HADOOP_HOME/bin $JPS -O $HADOOP_HOME/bin/jpsall
 chmod +x $HADOOP_HOME/bin/jpsall
 
 
-MyHadoop="https://ghproxy.com/https://raw.githubusercontent.com/WangJiu-czy/bigdata-shell/main/init/myhadoop.sh"
+MyHadoop="https://ghproxy.com/https://raw.githubusercontent.com/WangJiu-czy/bigdata-shell/main/init/myhadoop.sh" --no-check-certificate
 wget -q -P $HADOOP_HOME/bin $MyHadoop -O $HADOOP_HOME/bin/myhadoop.sh
 chmod +x $HADOOP_HOME/bin/myhadoop.sh
 printf "\n"
